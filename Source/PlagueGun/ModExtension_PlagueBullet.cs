704774abb1cc223b18c21a4ffd7cf7782c2b4798
rosebeal <rose.beal1998@gmail.com>
2023-06-24 08:50:47 +0100

    First commit

---
+++ Source/PlagueGun/ModExtension_PlagueBullet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace RS_PlagueGun
+{
+    public class ModExtension_PlagueBullet : DefModExtension
+    {
+        public float addHediffChance = 0.05f;
+        public HediffDef hediffToAdd;
+    }
+
+    public class ModExtension_FireBullet : DefModExtension
+    {
+        //add to xml!!
+        public int fireSize = 1;
+    }
+}
---
+++ Source/PlagueGun/RS_Bullet.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace RS_PlagueGun
+{
+    public class Projectile_PlagueBullet : Bullet
+    {
+        public ModExtension_PlagueBullet Props => def.GetModExtension<ModExtension_PlagueBullet>();
+
+        protected override void Impact(Thing hitThing, bool blockedByShield = false)
+        {
+            base.Impact(hitThing, blockedByShield);
+
+            if (Props != null && hitThing != null && hitThing is Pawn hitPawn)
+            {
+                float rand = Rand.Value;
+                if (rand <= Props.addHediffChance)
+                {
+                    Messages.Message("RS_PlagueBullet_SuccessMessage".Translate(
+                        this.launcher.Label, hitPawn.Label
+                    ), MessageTypeDefOf.NeutralEvent);
+                    Hediff plagueOnPawn = hitPawn.health?.hediffSet?.GetFirstHediffOfDef(Props.hediffToAdd);
+                    float randomSeverity = Rand.Range(0.15f, 0.30f);
+                    if (plagueOnPawn != null)
+                    {
+                        plagueOnPawn.Severity += randomSeverity;
+                    }
+                    else
+                    {
+                        Hediff hediff = HediffMaker.MakeHediff(Props.hediffToAdd, hitPawn);
+                        hediff.Severity = randomSeverity;
+                        hitPawn.health.AddHediff(hediff);
+                    }
+                }
+                
+            }
+        }
+    }
+
+    public class Projectile_FireBullet : Bullet
+    {
+        public ModExtension_FireBullet Props => def.GetModExtension<ModExtension_FireBullet>();
+
+        protected override void Impact(Thing hitThing, bool blockedByShield = false)
+        {
+            base.Impact(hitThing, blockedByShield);
+
+            if(Props != null)
+            {
+                if (hitThing != null && hitThing is Pawn hitPawn)
+                {
+                    hitPawn.TryAttachFire(Props.fireSize);
+                    
+                }
+                //if it hits something thats not a pawn
+                else if (hitThing != null) 
+                {
+                    FireUtility.TryAttachFire(hitThing,Props.fireSize);
+                }
+            }
+            
+            
+            
+        }
+    }
+}
